e99dd305211f74571743c79c936b4c31297297b8
hbjs97 <hbjs97@naver.com>
2022-11-16 05:52:22 +0900

    테스트 로깅 제거

--- src/main/kotlin/com/herecity/HereCityApplication.kt
+++ src/main/kotlin/com/herecity/HereCityApplication.kt
@@ -1,12 +1,8 @@
 package com.herecity
 
-import mu.KotlinLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-
-
-private val logger = KotlinLogging.logger {}
 
 
 @SpringBootApplication
@@ -14,9 +10,5 @@ private val logger = KotlinLogging.logger {}
 class HereCityApplication
 
 fun main(args: Array<String>) {
-  logger.info("1")
-  logger.info("2")
   runApplication<HereCityApplication>(*args)
-  logger.info("3")
-  logger.info("4")
 }
