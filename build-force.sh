8df50ee50c9f52eb9f52196e63f76238509a777a
hbjs97 <hbjs97@naver.com>
2023-03-31 23:48:16 +0900

    etc - SPRING_PROFILES_ACTIVE to prod

--- build-force.sh
+++ build-force.sh
@@ -1,5 +1,6 @@
 # ps -ef | grep java
 # kill <PID>
 
+export SPRING_PROFILES_ACTIVE=prod
 ./gradlew build -x detekt -x test
 nohup java -jar build/libs/here-city-backend-0.0.1-SNAPSHOT.jar > app.log &
