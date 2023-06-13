ec733b15270db47931de115efd1e5fc9908efec7
hbjs97 <hbjs97@naver.com>
2023-06-14 02:06:19 +0900

    fix - 회원가입 및 로그인

--- src/main/kotlin/com/herecity/user/adapter/input/request/SignInRequest.kt
+++ src/main/kotlin/com/herecity/user/adapter/input/request/SignInRequest.kt
@@ -5,8 +5,6 @@ import com.herecity.user.application.port.input.SignInQuery
 data class SignInRequest(
     val email: String,
     val password: String,
-    val displayName: String,
-    val thumbnail: String?,
 ) {
     fun toDomain(): SignInQuery.In = SignInQuery.In(
         email = email,
